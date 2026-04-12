4dbf8c2fb68aa5236299db649ede07de9679c000
yelobat <lukeanthonyholland@gmail.com>
2026-04-12 19:07:23 +0100

    added a doom emacs config.

---
+++ config/doom/custom.el
@@ -0,0 +1,23 @@
+(put 'customize-themes 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("ecea000f200655349e91cf67d889357429cb15772a138a570a4673dae0e98e69"
+     "984eda6bd3dbea79f3e7d36ea573f50541cf286ad2c46f6c838ed08c512f76d3"
+     "d33fa4c5909b87f904e12fe67b3c70f9c66e3e0754c93ba3835153bccc2bb6a7"
+     "756aae7559b26abaeb06bced08a2b1200e5eb6a22caf963bdba6d502e9db1cf6"
+     "dde31de5494132a33ca90d8e287c59020d02ff223dd492333385cfd3518c8858"
+     "cabfc272f015b9318913c32641faa5768e1682d0e0863d23a4b45a95fc68dddf"
+     "c3257208cc91a38df1ea146f0248ecd60e772b6c1ada3be5924a7823b9baf1da"
+     "2e82fa430cf5192142736e286c1d5a531f784b560761c1d8c1cd13de0babc82a"
+     "dec3fb7afb4097c730bd8f4b232b460a3132804e04a23e8c05f33d4d519291e8"
+     "dfdc20301b443ca5b1a648fa9487ace8193757b11730bad55dd4f6646f63b385" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
